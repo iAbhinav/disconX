017c928581ea2369d3cd177941029cdd7f4cda71
Abhinav <abhisinghal1087@gmail.com>
2024-03-28 22:21:23 +0530

    ✅ Init Project. ✅ Content.js injection ✅ Popup.js injection ✅ Button click working

---
+++ content.js
@@ -0,0 +1,9 @@
+function helloWorld(){
+    console.log("Hello World")
+}
+
+helloWorld();
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//     console.log("Button clicked in popup!")
+// });
---
+++ popup.js
@@ -0,0 +1,16 @@
+document.getElementById("startDisconnectBtn").addEventListener("click", () => {
+  console.error("Button Clicked!");
+//
+//   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//     chrome.tabs.sendMessage(tabs[0].id, { action: "scrollToBottom" }, function (response) {
+//       console.error("Message Sent")
+//     });
+//   });
+});
+
+
+function helloPop(){
+  console.error("Hello Pop")
+}
+
+helloPop();

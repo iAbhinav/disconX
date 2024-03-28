868286a279434eca320ecf3ab825080adf120d86
Abhinav <abhisinghal1087@gmail.com>
2024-03-29 00:58:42 +0530

    Scrolling to the bottom of following list.

--- content.js
+++ content.js
@@ -1,9 +1,18 @@
-function helloWorld(){
-    console.log("Hello World")
-}
-
-helloWorld();
+var previousHeight = 0;
+const scrollToBottom = () => {
+    if(previousHeight == document.documentElement.scrollHeight) {
+        console.log("End")
+        previousHeight = 0;
+    } else {
+        console.log("Go More")
+        window.scrollTo(previousHeight, document.documentElement.scrollHeight);
+        previousHeight = document.documentElement.scrollHeight;
+        setTimeout(scrollToBottom, 2000)
+    }
+};
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log("Button clicked in popup!")
+    if (request["action"] === "scrollToBottom") {
+        scrollToBottom();
+    }
 });
--- popup.js
+++ popup.js
@@ -1,16 +1,7 @@
 document.getElementById("startDisconnectBtn").addEventListener("click", () => {
-  console.error("Button Clicked!");
-
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { action: "scrollToBottom" }, function (response) {
-      console.error("Message Sent")
+      // console.error("Message Sent")
     });
   });
 });
-
-
-function helloPop(){
-  console.error("Hello Pop")
-}
-
-helloPop();
